ac63f268ea53197347922c1b5d2d62a36ed2fe40
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-09-15 11:36:23 +0900

    fix: resolve final Box component TypeScript issue in QACard

--- libs/components/community/QACard.tsx
+++ libs/components/community/QACard.tsx
@@ -74,8 +74,7 @@ const QACard = (props: QACardProps) => {
 				{/* Header with user info and type indicator */}
 				<Stack direction="row" alignItems="center" justifyContent="space-between" mb={2}>
 					<Stack direction="row" alignItems="center" spacing={2}>
-						<Box
-													
+						<Box component="div"
 							sx={{
 								width: 40,
 								height: 40,
